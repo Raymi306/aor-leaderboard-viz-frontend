8a58a445257a057475edde461cfaec9c88831e61
Marco Arriaga <marriaga158@gmail.com>
2022-06-12 17:11:34 -0400

    added the donut chart (#4)
    add car choice donut chart component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import LineChart from './LineChartAllTimes';
 import ScrollableLeaderboard from './ScrollableLeaderboard';
+import DonutChart from './components/DonutChart';
 import { genLeaderboardEntries } from './mock-data';
 
 function App() {
+  let leaderboardEntries = genLeaderboardEntries(10); // for consistency in data output
+
   return (
     <div className="App">
-	<ScrollableLeaderboard incoming_data={genLeaderboardEntries(10)}></ScrollableLeaderboard>
+	    <ScrollableLeaderboard incoming_data={leaderboardEntries}></ScrollableLeaderboard>
+      <DonutChart incoming_data={leaderboardEntries}/>
     </div>
   );
 }
---
+++ src/components/DonutChart.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import type { LeaderboardEntry } from '../types';
+// TODO: remove what's unneeded from here
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Legend,
+  Title,
+  Tooltip
+} from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+type Props = {}
+
+ChartJS.register(ArcElement); // this needs to exist for some reason
+ChartJS.register(Legend);
+ChartJS.register(Tooltip);
+ChartJS.register(Title);
+
+const options = {
+  legend: {
+    display: true,
+  },
+};
+
+let chartColors = ["#CCCCCC",
+"#669999",
+"#CCCC66",
+"#CC6600",
+"#9999FF",
+"#0066CC",
+"#99CCCC",
+"#999999",
+"#FFCC00",
+"#009999",]
+
+const DonutChart = (props: any) => { // not sure what props: any *really* does, I'm a typescript noob
+  let cars = new Map<string, number>(); // maps car name : # of appearances
+  for(let entry of props.incoming_data){
+    // cars.set(entry.car, cars.get(entry.car)?+1 : 1); // if it exists, increment else set it to 1
+    cars.set(entry.car, (cars.get(entry.car) ?? 0) + 1)
+  }
+
+  // convert the data to something usable by chartjs
+  let data = {
+    datasets: [
+      {
+        label: 'car usage',
+        data: Array.from(cars.values()),
+        backgroundColor: chartColors,
+        hoverOffset: 4
+      }
+    ],
+    labels: Array.from(cars.keys())
+  }
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+  }  
+
+  return (
+    <div>
+      <Doughnut data={data} />
+    </div>
+  )
+}
+
+export default DonutChart
