12995bd30f2da5e7dcba2fe245adb1d2f9176dad
Andrew Langmeier <raymi306@gmail.com>
2022-06-20 15:45:21 -0400

    Add key to iterable element

--- src/components/ScrollableLeaderboard.tsx
+++ src/components/ScrollableLeaderboard.tsx
@@ -7,7 +7,7 @@ export default function ScrollableLeaderboard(props: any) {
 	const leaderboardEntryCards = [];
 	for (let entry of props.incoming_data) {
 		leaderboardEntryCards.push(
-				<div className="leaderboard-entry-card">
+				<div className="leaderboard-entry-card" key={entry.rank}>
 					<div className="rank-and-name section">{entry.rank}. {entry.name}</div>
 					<div className="car section">{entry.car}</div>
 					<div className="time-diff section">{scoreToTimeString(entry.score)}</div>
